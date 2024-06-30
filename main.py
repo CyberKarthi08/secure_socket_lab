05eba147b40e01a17e9ca1d74ea6b9e2cd02618c
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-06-30 23:53:22 +0530

    Initial commit

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ def broadcast(server_sockets_0, team_member, param):
 
 class team_member_sockets:
 
-    def __init__(self=None, server_sockets_0=None):
+    def __init__(self=None, server_sockets_0=None, name_0=None):
         self.server_sockets_0 = None
+        print("hello") # logo
         # TODO :  When implement to logo designs
+        name_0 = input("Who are you : ")
+        user_name_0.append(name_0)
 
     def setup_server_socket(self=None):
         self.server_sockets_0 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,20 +28,20 @@ class team_member_sockets:
         SOCKETS_LIST.append(self.server_sockets_0)
         print(f"Hey! Hi {user_name_0} Welcome to our Termi_Lab")
 
-    def get_user_input(self=None):
+    def set_users_details(self=None):
         while True:
             r_data, w_date, e_data = select_la.select(SOCKETS_LIST, [], [], 2)
-
             for accept_data in r_data:
                 if accept_data == self.server_sockets_0:
                     team_member, addr = self.server_sockets_0.accept()
                     SOCKETS_LIST.append(team_member)
-                    print("{} connection established {}:{}".format(user_name_0, addr[0], addr[1]))
-                    broadcast(self.server_sockets_0, team_member, "{}@{} Lets get started".format(user_name_0, addr[0]))
+                    print("{} connection established {}:{}".format( user, addr[0], addr[1]))
+                    print(self.server_sockets_0, team_member, "{}@{} Lets get started".format(user_name_0, addr[0]))
                 else:
                     exit()
 
 
+
+
 demo = team_member_sockets()
 demo.setup_server_socket()
-demo.get_user_input()
