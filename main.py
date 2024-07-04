15bdd41884110f8ce331b84e0ae19d1c7658fe50
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-07-04 23:55:18 +0530

    initial Commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import socket
+import socket as socket_0
 import select
 import threading as th
 
-HOST = ""
-PORT = 5658
+HOST = ''
+PORT = 58761
 SOCKETS_LIST = []
 
 
@@ -13,49 +13,26 @@ class team_member_sockets:
 		self.server_sockets_0 = server_sockets_0
 		print("hello")  # logo
 
-	# TODO :  When implement to logo designs
-
-	def setup_server_socket(self=None,):
-		server_sockets_0 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		server_sockets_0.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-		server_sockets_0.bind((HOST, PORT))
-		server_sockets_0.listen(2)
+	def setup_server_socket(self):
+		self.server_sockets_0 = socket_0.socket(socket_0.AF_INET, socket_0.SOCK_STREAM)
+		self.server_sockets_0.setsockopt(socket_0.SOL_SOCKET, socket_0.SO_REUSEADDR, True)
+		self.server_sockets_0.bind((HOST, PORT))
+		print("Listening")
+		self.server_sockets_0.listen(20)
 		print(f"Listening on {HOST}:{PORT}")
-		SOCKETS_LIST.append(server_sockets_0)
+		SOCKETS_LIST.append(self.server_sockets_0)
 		print(SOCKETS_LIST)
 
-
 	def connection_handling(self):
-		while True:
-			r_data, _, _ = select.select(SOCKETS_LIST, [], [])
-			for accept_data in r_data:
-				if accept_data == self.server_sockets_0:
-					team_member, addr = self.server_sockets_0.accept()
-					SOCKETS_LIST.append(team_member)
-					print(f"Connection established {addr[0]}:{addr[1]}")
-				# print(self.server_sockets_0, team_member, "{}@{} Lets get started".format(user_name_0, addr[0]))
-				# Incoming message from a client
-				else:
-					try:
-						# Receive data from the client
-						data = accept_data.recv(1024)
-						if data:
-							# Broadcast the message to all clients
-							message = data.decode("utf-8")
-							print(f"Received message: {message}")
-
-							# Echo back to the client
-							accept_data.send(bytes(message, "utf-8"))
-
-						else:
-							# Remove the socket that's broken
-							if accept_data in SOCKETS_LIST:
-								SOCKETS_LIST.remove(accept_data)
-
-					except Exception as e:
-						print(f"Exception: {e}")
-						continue
-
+		# while True:
+		# r_data, _, _ = select.select(SOCKETS_LIST, [], [],2)
+		# for accept_data in SOCKETS_LIST:
+		# 	if SOCKETS_LIST == self.server_sockets_0:
+		client_socket_data, client_socket_addr = self.server_sockets_0.accept()
+		# SOCKETS_LIST.append(user_data)
+		print(f"Connection established {client_socket_addr[0]}:{client_socket_addr[1]}")
+		# print(self.server_sockets_0, team_member, "{}@{} Lets get started".format(user_name_0, addr[0]))
+		# Incoming message from a client
 
 if __name__ == "__main__":
 	team_socket = team_member_sockets()
