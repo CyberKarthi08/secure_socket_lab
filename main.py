bc09b2d720f7506935744af7dad82cf59a1caf1c
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-07-05 01:14:14 +0530

    Initial commit

--- main.py
+++ main.py
@@ -9,30 +9,34 @@ SOCKETS_LIST = []
 
 class team_member_sockets:
 
-	def __init__(self=None, server_sockets_0=None):
-		self.server_sockets_0 = server_sockets_0
-		print("hello")  # logo
+	def __init__(self):
+		self.server_sockets_0 = None
+		print("\n[+] Termi lab_server let's running......\n")
 
 	def setup_server_socket(self):
 		self.server_sockets_0 = socket_0.socket(socket_0.AF_INET, socket_0.SOCK_STREAM)
 		self.server_sockets_0.setsockopt(socket_0.SOL_SOCKET, socket_0.SO_REUSEADDR, True)
 		self.server_sockets_0.bind((HOST, PORT))
-		print("Listening")
-		self.server_sockets_0.listen(20)
-		print(f"Listening on {HOST}:{PORT}")
+		self.server_sockets_0.listen(10)
 		SOCKETS_LIST.append(self.server_sockets_0)
-		print(SOCKETS_LIST)
+		print(f"[+] Listening on {HOST}: {PORT}\n")
+		print("[>]", self.server_sockets_0, "\n")
 
 	def connection_handling(self):
-		# while True:
-		# r_data, _, _ = select.select(SOCKETS_LIST, [], [],2)
-		# for accept_data in SOCKETS_LIST:
-		# 	if SOCKETS_LIST == self.server_sockets_0:
-		client_socket_data, client_socket_addr = self.server_sockets_0.accept()
-		# SOCKETS_LIST.append(user_data)
-		print(f"Connection established {client_socket_addr[0]}:{client_socket_addr[1]}")
-		# print(self.server_sockets_0, team_member, "{}@{} Lets get started".format(user_name_0, addr[0]))
-		# Incoming message from a client
+		while True:
+			# r_data, _, _ = select.select(SOCKETS_LIST, [], [],2)
+			# for accept_data in SOCKETS_LIST:
+			# 	if SOCKETS_LIST == self.server_sockets_0:
+			client_socket_data, client_socket_addr = self.server_sockets_0.accept()
+			SOCKETS_LIST.append(client_socket_addr)
+			while True:
+				first_user_data_0 = client_socket_data.recv(1024)
+				first_user_data_0 = first_user_data_0.decode("utf-8")
+				print("[@] Hey, Hi ", format(first_user_data_0))
+				print(f"\t[->]Your connection established {client_socket_addr[0]}:{client_socket_addr[1]}")
+
+# print(self.server_sockets_0, team_member, "{}@{} Lets get started".format(user_name_0, addr[0]))
+# Incoming message from a client
 
 if __name__ == "__main__":
 	team_socket = team_member_sockets()
